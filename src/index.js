bd2c9780e0695a9dadd83c22c23cb74b07f99cd0
Maksim Valiakine <mvaliakine@tourmalinecore.com>
2022-10-23 20:23:42 +0500

    feat(Cards): #DsxkVmOy: add Cards. Change styles for Header and Card

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import { render, RenderPosition } from './utils/render';
+import CardsView from './view/CardsView/CardsView';
 import HeaderView from './view/HeaderView/HeaderView';
 import HeroBlockView from './view/HeroBlockView/HeroBlockView';
+
+const cards = [
+  {
+    src: './images/cards/card-1.svg',
+    title: 'Bring Technology To Your Comfrotable Home',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    className: 'cards-view__image',
+  },
+  {
+    src: './images/cards/card-2.svg',
+    title: 'Make Your business To Be Better Famous In Internet',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+  {
+    src: './images/cards/card-3.svg',
+    title: 'Build Your Digital Product That Suitable For Your Need',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+];
 
 const body = document.querySelector('.body');
 const main = document.querySelector('.main');
@@ -11,3 +31,7 @@ render(body, Header, RenderPosition.AFTERBEGIN);
 const HeroBlock = new HeroBlockView();
 render(main, HeroBlock, RenderPosition.BEFOREEND);
 HeroBlock.renderButton();
+
+const Cards = new CardsView(cards);
+render(main, Cards, RenderPosition.BEFOREEND);
+Cards.renderCards();
